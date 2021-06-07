a6f9ca584d80b78ea1e011342d66bc3af3f546cf
Vincent Odhiambo Okoth <77384003+vincentokoth@users.noreply.github.com>
2021-06-07 16:48:02 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+import csvdata_table from "https://github.com/vincentokoth/vincentokoth.github.io/blob/master/csvdata_table.js"
 
+const tableRoot = document.querySelector("#https://github.com/vincentokoth/vincentokoth.github.io/blob/master/JKUAT(GEGIS)-Projects.csv");
+const table csvdata_table = new csvdata_table(tableRoot); 
+
+csvdata_table.setHeader(["1st name", "2nd name", "Course", "Supervisor", "Project Title" ]);
