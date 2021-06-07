aff01139051ab694f1de17c449be6fbbb94ac869
Vincent Odhiambo Okoth <77384003+vincentokoth@users.noreply.github.com>
2021-06-07 16:48:29 +0300

    Update csvdata_table.js

--- csvdata_table.js
+++ csvdata_table.js
@@ -1,1 +1,18 @@
-
+export default class {
+   /**
+    * @param {HTMLTableElement} root the table element which will display the csv data.
+    */
+  constructor(root) {
+    this.root = root; 
+  }
+  /**
+    * @param {string[]} header columns List of headings to be used.
+    */
+  setHeader(HeaderColumns) {
+    this.root.insertAdjacentHTML("afterbegin", ' 
+                                 <thead>
+                                    ${headerColumns.map(text => '<th>${text}</th>').join("")}
+                                 </thead>
+                                 ');
+  }
+}
